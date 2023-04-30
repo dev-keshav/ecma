8c8b1acf196e976258c78e85a54f73e85ca3e27f
dev-keshav <keshavkumar9431@gmail.com>
2023-05-01 03:08:04 +0530

    3rd commit

---
+++ arrowFunction.js
@@ -0,0 +1,10 @@
+let data = [23, 34, 11, 9, 35, 69, 31, 80, 84, 104, 22];
+
+let normalFun = data.map(function(idx){
+    return idx*3;
+})
+console.log(normalFun);
+
+
+let arrorFun = data.map((item)=>item*4)
+console.warn(arrorFun)
--- find_findindex.js
+++ find_findindex.js
@@ -1,6 +1,39 @@
 let data = [3, 18, 11, 15, 20, 59, 69];
-// return item<10;
-let result = data.find(function(i){
-    return i<10;
-})
+
+let result = data.find((item) => item > 50 && item < 100)
 console.log(result);
+
+var users = [
+    {
+        id: 01,
+        name: 'Rahul',
+        type: 'dev'
+    },
+    {
+        id: 02,
+        name: 'Aditya',
+        type: 'dev'
+    },
+    {
+        id: 03,
+        name: 'Vaibhav',
+        type: 'dev'
+    },
+    {
+        id: 04,
+        name: 'Devianshi',
+        type: 'dev'
+    },
+    {
+        id: 05,
+        name: 'Khushi',
+        type: 'SW'
+    },
+]
+
+let res = users.find(function(item){
+    if(item.id>3){
+        return item.id;
+    }
+})
+console.log(res);
