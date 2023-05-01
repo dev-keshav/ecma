ec252faf45b445b0e2bb4e7b9d12427ffdc5f1b8
dev-keshav <keshavkumar9431@gmail.com>
2023-05-02 02:55:43 +0530

    6th commit

--- lexicalScope.js
+++ lexicalScope.js
@@ -3,7 +3,18 @@ let data = {
     friends: ['Adi', 'Vaibhav', 'Rahul', 'Dev', 'Teej'],
     age: [32, 32, 22, 21, 45],
     getHai:function(){
-        console.log(this.friends)
+        // console.log(this.list);
+
+        // ------- By using this with normal function -------
+        var that = this.list
+        this.friends.map(function(item){
+            console.log(that, item)
+        })
+
+        // --------- By using this with arrow function -------
+        this.friends.map((item) => {
+            console.log(this.list, item)
+        })
     }
 }
 
