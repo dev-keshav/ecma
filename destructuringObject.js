75aa605d5f5b42693d48c5ab42635aedaee64313
dev-keshav <keshavkumar9431@gmail.com>
2023-05-03 01:35:49 +0530

    8th commit

---
+++ destructuringObject.js
@@ -0,0 +1,9 @@
+let users = {
+    name: 'Anil',
+    email: 'anil@gmail.com',
+    mobile: 754389749
+}
+let {email, name, mob=89878}=users;
+console.log(name);
+console.log(email);
+console.log(mob);
---
+++ spreadOperator.js
@@ -0,0 +1,15 @@
+let otherfruits = ['Pineapple', 'Lichi'];
+let fruits=["Apple", 'orange', ...otherfruits, 'mango', 'kiwi'];
+
+function printAll(a, b, c, d){
+    console.log(a,b,c,d);
+}
+function printAll2(...a){       // rest operator
+    console.log(a);
+}
+
+// without using spread operator 
+printAll(fruits[0], fruits[1], fruits[2], fruits[3]);      
+
+// using spread operator
+printAll2(...fruits);      // spread operator
